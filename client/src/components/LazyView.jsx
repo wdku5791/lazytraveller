28adb0ec685440f04f2b28b85c626893542756c2
Winston Ku <wdku5791@gmail.com>
2017-03-27 14:50:43 -0700

    adds some map interaction when clicking list or markers.

--- client/src/components/LazyView.jsx
+++ client/src/components/LazyView.jsx
@@ -7,11 +7,29 @@ export default class LazyView extends React.Component {
   	super(props);
 
     this.state = {
-      map: ''
+      map: '',
+      selected: '',
+      markerList: [],
+      initialMarker: {
+        path: google.maps.SymbolPath.CIRCLE,
+        scale: 4,
+        // fillColor: 'white',
+        // fillOpacity: 1.0,
+        strokeColor: 'red',
+
+      },
+      selectedMarker: {
+        path: google.maps.SymbolPath.CIRCLE,
+        scale: 8,
+        fillColor: 'white',
+        fillOpacity: 1.0,
+        strokeColor: 'cyan',
+      }
     }
 
     this.filterData = this.filterData.bind(this);
     this.placeMarkers = this.placeMarkers.bind(this);
+    this.selectionHandler = this.selectionHandler.bind(this);
   }
 
   componentDidMount() {
@@ -35,27 +53,49 @@ export default class LazyView extends React.Component {
   }
 
   placeMarkers(places) {
+    let markerArray = [];
     places.forEach((place, index) => {
       let marker = new google.maps.Marker({
         position: {
           lat: place._activity.location.latitude,
           lng: place._activity.location.longitude
         },
-        icon: {
-          path: google.maps.SymbolPath.CIRCLE,
-          scale: 4
-        },
+        icon: this.state.initialMarker,
         map: this.state.map,
-        label: `${index+1}`,
-        title: 'Hello world'
+        label: {
+          text: `${index+1}`,
+          fontSize: '22px',
+        },
+        title: 'Hello world',
       })
+      
+      let context = this;
+      
       marker.addListener('click', function(event) {
-        marker.setIcon({
-          path: google.maps.SymbolPath.CIRCLE,
-          scale: 6
-        })
+        console.log(this);
+        console.log(context.state.markerList);
+        if (context.state.selected === '') {
+          context.setState({ selected: this.label.text });
+        } else {
+          let state = context.state
+          state.markerList[state.selected - 1].setIcon(context.state.initialMarker);
+          context.setState({ selected: this.label.text });
+        }
+        marker.setIcon(context.state.selectedMarker);
       })
+      markerArray.push(marker);
     })
+    this.setState({ markerList: markerArray });
+  }
+
+  selectionHandler(selectionId) {
+    if (this.state.selected === '') {
+      this.setState({ selected: selectionId });  
+    } else {
+      this.state.markerList[this.state.selected - 1].setIcon(this.state.initialMarker);
+      this.setState({ selected: selectionId });
+    }
+    this.state.markerList[selectionId - 1].setIcon(this.state.selectedMarker);
   }
 
   filterData(type) {
@@ -72,7 +112,9 @@ export default class LazyView extends React.Component {
           <div id="map" style={{height: '100%'}}></div>
         </div>
         <LazyList 
-          data={ this.props.data } />
+          data={ this.props.data }
+          state={ this.state } 
+          selector = { this.selectionHandler }/>
       </div>
     )
   }
